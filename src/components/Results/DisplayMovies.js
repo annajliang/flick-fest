7188af87611510ea161194468a84330eb63a6eea
annajliang <annaliangdev@gmail.com>
2020-09-02 00:28:17 -0400

    add css grids to movie results, resize poster images

--- src/components/Results/DisplayMovies.js
+++ src/components/Results/DisplayMovies.js
@@ -1,16 +1,17 @@
 import React from "react";
 import NominateBtn from "../Buttons/NominateBtn";
 import PropTypes from 'prop-types';
+import "./results.css";
 
 const DisplayMovies = (props) => {
   return (
     <div>
       <h2>Movies Searches:</h2>
-      <ul>
+      <ul className="gridContainer">
         {props.movies.map(({ Title, Year, imdbID, Poster }) => {
           return (
             <li key={imdbID}>
-              <img src={props.moviePoster(Poster)} alt="Poster of movie" />
+              <img src={props.moviePoster(Poster)} alt="Poster of movie" className="posterSize" />
               <h2>{Title}</h2>
               <p>{Year}</p>
               <NominateBtn
--- src/components/Results/NominatedMovies.js
+++ src/components/Results/NominatedMovies.js
@@ -1,16 +1,17 @@
 import React from "react";
 import RemoveBtn from "../Buttons/RemoveBtn";
 import PropTypes from 'prop-types';
+import "./results.css";
 
 const NominatedMovies = (props) => {
   return (
     <div>
       <h2>Nominated Movies:</h2>
-      <ul>
+      <ul className="gridContainer">
         {props.nominatedMovies.map(({ Title, Year, imdbID, Poster }) => {
           return (
             <li key={imdbID}>
-              <img src={props.moviePoster(Poster)} alt="Poster of movie" />
+              <img src={props.moviePoster(Poster)} alt="Poster of movie" className="posterSize" />
               <h2>{Title}</h2>
               <p>{Year}</p>
               <RemoveBtn
