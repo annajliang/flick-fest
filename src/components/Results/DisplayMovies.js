cf0b81a481d482848b9020edd7498313633667d1
annajliang <annaliangdev@gmail.com>
2020-09-03 03:02:31 -0400

    remove unused svg tag from displayMovies

--- src/components/Results/DisplayMovies.js
+++ src/components/Results/DisplayMovies.js
@@ -8,7 +8,6 @@ import divider from "../../assets/resultsDivider.svg";
 const DisplayMovies = (props) => {
   return (
     <section className={styles.container}>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#F1FAFD" fill-opacity="1" d="M0,128L48,128C96,128,192,128,288,117.3C384,107,480,85,576,90.7C672,96,768,128,864,154.7C960,181,1056,203,1152,202.7C1248,203,1344,181,1392,170.7L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
       <div className="wrapper">
         <h2>Pick Your 5 Faves!</h2>
         <img src={divider} alt="" className={styles.divider} />
