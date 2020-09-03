410b538730d51bbe88085509536ed4ea6353e18f
annajliang <annaliangdev@gmail.com>
2020-09-03 04:22:44 -0400

    draft movie card styles

--- src/components/Results/DisplayMovies.js
+++ src/components/Results/DisplayMovies.js
@@ -14,18 +14,24 @@ const DisplayMovies = (props) => {
         <ul className="gridContainer">
           {props.movies.map(({ Title, Year, imdbID, Poster }) => {
             return (
-              <li key={imdbID}>
-                <img src={props.moviePoster(Poster)} alt="Poster of movie" className="posterSize" />
-                <h3>{Title}</h3>
-                <p>{Year}</p>
-                <NominateBtn
-                  text="Nominate"
-                  nominateMovie={props.nominateMovie}
-                  isDisabled={props.nominatedMoviesIds.includes(imdbID)}
-                  movieId={imdbID}
-                  movieTitle={Title}
-                  movieYear={Year}
-                />
+              <li key={imdbID} className="movieCard">
+                <div className="movieCardThumbnail">
+                  <img src={props.moviePoster(Poster)} alt="Poster of movie" className="posterSize" />
+                </div>
+                <div className="movieCardContent">
+                  <div className="movieTitle">
+                    <h3>{Title}</h3>
+                  </div>
+                  <p className="movieYear">{Year}</p>
+                  <NominateBtn
+                    text="Nominate"
+                    nominateMovie={props.nominateMovie}
+                    isDisabled={props.nominatedMoviesIds.includes(imdbID)}
+                    movieId={imdbID}
+                    movieTitle={Title}
+                    movieYear={Year}
+                  />
+                </div>
               </li>
             );
           })}
