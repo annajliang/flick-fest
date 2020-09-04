761aec60595b2bab245d2f6117710b7fb7ca1908
annajliang <annaliangdev@gmail.com>
2020-09-04 02:23:40 -0400

    redo styling for nominated movie cards in sidebar

--- src/components/Results/NominatedMovies.js
+++ src/components/Results/NominatedMovies.js
@@ -1,29 +1,31 @@
 import React from "react";
 import RemoveBtn from "../Buttons/RemoveBtn";
 import PropTypes from 'prop-types';
-import "./results.css";
+import styles from "./NominatedMovies.module.css";
 
 const NominatedMovies = (props) => {
   return (
-    <section>
-      <h2>Nominated Movies:</h2>
-      <ul className="gridContainer">
+    <section className={styles.container}>
+      <h2>Nominees List</h2>
+      <ul className={styles.gridContainer}>
         {props.nominatedMovies.map(({ Title, Year, imdbID, Poster }) => {
           return (
-            <li key={imdbID} className="movieCard">
-              <div className="movieCardThumbnail">
+            <li key={imdbID} className={styles.movieCard}>
+              <div className={styles.movieCardThumbnail}>
                 <img src={props.moviePoster(Poster)} alt="Poster of movie" className="posterSize" />
               </div>
-              <div className="movieCardContent">
-                <div className="movieTitle">
+              <div className={styles.movieCardContent}>
+                <div className={styles.movieTitle}>
+                   <p className={styles.movieYear}>Released in {Year}</p>
                   <h3>{Title}</h3>
                 </div>
-                <p className="movieYear">{Year}</p>
-                <RemoveBtn
-                  text="Remove"
-                  removeMovie={props.removeMovie}
-                  movieId={imdbID}
-                />
+                <div className={styles.btnContainer}>
+                  <RemoveBtn
+                    text="Remove"
+                    removeMovie={props.removeMovie}
+                    movieId={imdbID}
+                  />
+                </div>
               </div>
             </li>
           );
