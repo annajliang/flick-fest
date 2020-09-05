3a44bc05341d19e86ba2a3382394673cd5b6aab4
annajliang <annaliangdev@gmail.com>
2020-09-05 18:51:52 -0400

    separate header svg divider into its own component, delete unneeded assets, rename + edit assets

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
-import divider from "../../assets/headerDivider.svg";
+import wavesUnderline from "../../assets/wavesUnderline .svg";
+import HeaderDivider from "./HeaderDivider";
 import styles from "./Header.module.css";
 
 const Header = ({ scrollTo }) => {
   return (
     <header>
       <div className={`${styles.container}`}>
-        {/* <Navbar /> */}
         <div className="wrapper">
           <div className={styles.title}>
             <h1>The Shoppies</h1>
-            <img src={divider} alt="" className={styles.divider} />
+            <img src={wavesUnderline} alt="" className={styles.divider} />
             <div className={styles.introText}>
               <p className={styles.textMargin}>
                 Welcome to Shopify's 1st ever virtual movie awards.
@@ -41,13 +41,7 @@ const Header = ({ scrollTo }) => {
           </div>
         </div>
       </div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path
-          fill="#3fd0d4"
-          fill-opacity="1"
-          d="M0,256L48,250.7C96,245,192,235,288,192C384,149,480,75,576,80C672,85,768,171,864,170.7C960,171,1056,85,1152,42.7C1248,0,1344,0,1392,0L1440,0L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
-        ></path>
-      </svg>
+      <HeaderDivider />
     </header>
   );
 };
