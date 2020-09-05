accb45c71174d2388ede7c20e6bc94fa670ea285
annajliang <annaliangdev@gmail.com>
2020-09-05 16:33:28 -0400

    style sidebar close btn

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,11 +1,19 @@
 import React from "react";
 // import NominatedMovies from "./components/Results/NominatedMovies";
+import CloseBanner from "../Banner/CloseBanner";
 import styles from "./Sidebar.module.css";
 
 const Sidebar = ({ children, isSidebarOpened }) => {
   return (
     <div className={`${styles.slidingSidebar} ${isSidebarOpened ? styles.show : styles.hide}`}>
       {children}
+      <span
+      className={styles.closeSidebar}
+      tabIndex="0"
+      role="button"
+      aria-label="Close"
+      // onClick={closeBanner}
+    ></span>
     </div>
   );
 };
