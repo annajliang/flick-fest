cb5cc34691bd7e7009905d66503f4dabf26d8174
annajliang <annaliangdev@gmail.com>
2020-09-05 15:12:56 -0400

    style notification alert for when user adds film

--- src/components/Buttons/ViewNomineesBtn.js
+++ src/components/Buttons/ViewNomineesBtn.js
@@ -1,10 +1,15 @@
 import React from "react";
 import styles from "./ViewNomineesBtn.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilm } from "@fortawesome/free-solid-svg-icons";
 
 const ViewNomineesBtn = ({ toggleSidebar }) => {
   return (
     <div className={styles.container}>
-      <button onClick={toggleSidebar} className={styles.viewNominees}>Your Nominees</button>
+      <button onClick={toggleSidebar} className={styles.viewNominees}>
+        <FontAwesomeIcon icon={faFilm} className={styles.filmIcon} />
+        <span className={styles.nomineeNotification}>0</span>
+      </button>
     </div>
   );
 };
