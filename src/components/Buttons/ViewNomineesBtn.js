833032bcbe7a2dadcec3811ee20aa00c410f4d9b
annajliang <annaliangdev@gmail.com>
2020-09-08 02:03:45 -0400

    attempt to make notification shake when movie is added

--- src/components/Buttons/ViewNomineesBtn.js
+++ src/components/Buttons/ViewNomineesBtn.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./ViewNomineesBtn.module.css";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilm } from "@fortawesome/free-solid-svg-icons";
 
 const ViewNomineesBtn = ({ toggleSidebar, nominatedMovies }) => {
+  const [isShaking, setIsShaking] = useState(false);
+
+  useEffect(() => {
+    if (nominatedMovies.length !== 0) {
+      setIsShaking(true);
+    }
+  }, [nominatedMovies]);
+
   return (
     <div className={styles.container}>
       <button onClick={toggleSidebar} className={styles.viewNominees}>
-        <FontAwesomeIcon icon={faFilm} className={styles.filmIcon} aria-label="view your nominations" />
+        <FontAwesomeIcon
+          icon={faFilm}
+          className={styles.filmIcon}
+          aria-label="view your nominations"
+        />
         {/* updates notification everytime a movie is nominated */}
-        <span className={styles.nomineeNotification}>
+        <span
+          className={`${styles.nomineeNotification} ${
+            isShaking ? styles.shake : undefined
+          }`}
+          key={nominatedMovies.length}
+        >
           {nominatedMovies.length}
         </span>
       </button>
